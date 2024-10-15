f1d78d99391367513a93575e031d46d1483d13f2
roshin26 <philip.roshin@gmail.com>
2024-10-15 21:29:58 +0100

    setting env vars

--- recipe_share/settings.py
+++ recipe_share/settings.py
@@ -15,15 +15,17 @@ from pathlib import Path
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
+if os.path.exists('env.py'):
+    import env
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-6-btw_^z&jof5yv*sa$t2e*9#1=xd&p*a#6b83++2=8jtw0#0z'
+SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = 'DEVELOPMENT' in os.environ
 
 ALLOWED_HOSTS = []
 
