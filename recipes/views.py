1cb1af483c4389bed441e6319892330b9b0b67df
roshin26 <philip.roshin@gmail.com>
2024-11-08 20:34:41 +0000

    search functionality

--- recipes/views.py
+++ recipes/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from django.views.generic import CreateView, ListView, DetailView, DeleteView,UpdateView
+from django.db.models import Q
 from django.contrib.auth.mixins import LoginRequiredMixin, UserPassesTestMixin
 
 from .models import Recipe
@@ -14,6 +15,20 @@ class Recipes(ListView):
     template_name = "recipes/recipes.html"
     model = Recipe
     context_object_name = "recipes"
+
+    def get_queryset(self, **kwargs):
+        query = self.request.GET.get('q')
+        if query:
+            recipes = self.model.objects.filter(
+                Q(title__icontains=query) |
+                Q(description__icontains=query) |
+                Q(instructions__icontains=query) |
+                Q(cuisine_types__icontains=query) |
+                Q(meal_type__icontains=query)
+            )
+        else:
+            recipes = self.model.objects.all()
+        return recipes
 
 
 class RecipeDetail(DetailView):
