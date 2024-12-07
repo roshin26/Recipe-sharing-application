23eba08299c69dac9e5d63f270083453dfdadfb7
EC2 Default User <ec2-user@ip-172-31-25-239.eu-west-1.compute.internal>
2024-12-07 19:17:16 +0000

    new ec2 instance

--- recipe_share/settings.py
+++ recipe_share/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = 'DEVELOPMENT' in os.environ
 
-ALLOWED_HOSTS = ['52.31.230.58']
+ALLOWED_HOSTS = ['54.194.116.168']
 
 
 # Application definition
