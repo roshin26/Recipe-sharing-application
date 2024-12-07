5f2911ce9ace63a3ecd9e4fefa240144150cbf7e
EC2 Default User <ec2-user@ip-172-31-21-65.eu-west-1.compute.internal>
2024-12-07 15:12:34 +0000

    from ec2

--- recipe_share/settings.py
+++ recipe_share/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = 'DEVELOPMENT' in os.environ
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['52.31.230.58']
 
 
 # Application definition
@@ -168,7 +168,8 @@ LOGIN_REDIRECT_URL= '/'
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATICFILES_DIRS =(os.path.join(BASE_DIR, 'static'),)
 
 #Cloudinary settings
