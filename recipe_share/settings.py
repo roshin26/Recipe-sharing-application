f3802335650d53acb286738cdbbdd74c5b65f54d
roshin26 <philip.roshin@gmail.com>
2024-12-09 10:38:04 +0000

    chnaging env

--- recipe_share/settings.py
+++ recipe_share/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = 'DEVELOPMENT' in os.environ
 
-ALLOWED_HOSTS = ['54.194.116.168']
+ALLOWED_HOSTS = os.environ.get('DJANGO_ALLOWED_HOSTS', '').split(',')
 
 
 # Application definition
